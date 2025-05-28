7d8de13d6d2b367c5ba9eda17999cea39d5d8156
ShivanshPahwa <shivanshpahwa7@gmail.com>
2025-05-29 03:17:27 +0530

    jsx heading

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement('h1', { id: 'heading' }, 'Hello without index.jsx');
-
+const heading2 = <h2>this is another heading</h2>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+// root.render(heading);
+root.render(heading2);
+
